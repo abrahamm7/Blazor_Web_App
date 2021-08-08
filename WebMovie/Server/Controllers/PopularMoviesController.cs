adc6bfd18078ba8c5179d0750de08cca5c5c764b
Abraham Morillo <abrahammorillo777@gmail.com>
2021-08-08 16:49:47 -0400

    Add project files.

---
+++ WebMovie/Server/Controllers/PopularMoviesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMovie.Server.Services;
+using WebMovie.Shared;
+
+namespace WebMovie.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PopularMoviesController : ControllerBase
+    {
+        readonly IPopularMovieServices _popularMovieServices;
+
+        public PopularMoviesController(IPopularMovieServices popularMovieServices)
+        {
+            _popularMovieServices = popularMovieServices;
+        }
+        
+        //public async Task<List<Result>> GetMovies()
+        //{
+        //    var movieList = _popularMovieServices.GetPopularMovies().Result;
+        //    return movieList;
+        //}
+
+        public async Task<IActionResult> GetMovies()
+        {
+            var movieList = _popularMovieServices.GetPopularMovies().Result;
+            return Ok(movieList);
+        }
+    }
+}
