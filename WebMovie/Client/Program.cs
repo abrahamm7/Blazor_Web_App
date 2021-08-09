1974332229fc64a3c6ac7af5578e409e3acb1a04
Abraham Morillo <abrahammorillo777@gmail.com>
2021-08-08 21:42:41 -0400

    some changes

--- WebMovie/Client/Program.cs
+++ WebMovie/Client/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.Modal;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,6 +21,7 @@ namespace WebMovie.Client
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<IPopularMovieServices, PopularMovieServices>();
+            builder.Services.AddBlazoredModal();
             await builder.Build().RunAsync();
         }
     }
